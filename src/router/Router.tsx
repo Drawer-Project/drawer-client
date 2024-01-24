6aab069b2b606ed2670bdfeee1024faf7ac1d3ec
Choi Hong Gyu <zxolqs@gmail.com>
2024-01-25 02:30:58 +0900

    create router for switching page

---
+++ src/router/Router.tsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import { Login } from "@/pages/login";
+import { Signup } from "@/pages/signup";
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+]);
+
+export { router };
