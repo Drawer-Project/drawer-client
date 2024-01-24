06041f7415af0fe8589d0dc46cb2243a7f21a5ff
Choi Hong Gyu <zxolqs@gmail.com>
2024-01-25 02:32:22 +0900

    apply router, query provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 
-import App from "./App.tsx";
+import { router } from "./router/Router";
+
 import "./index.css";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
