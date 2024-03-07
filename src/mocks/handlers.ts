5dfddc32e2cb0c4a54d7f1483462c4bc383504ff
Choi Hong Gyu <zxolqs@gmail.com>
2024-03-07 13:31:09 +0900

    add mockapi functions according to the addition of api

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,12 +2,28 @@ import { DefaultBodyType, http, HttpResponse, PathParams } from "msw";
 import { v4 as uuidv4 } from "uuid";
 
 import { LoginRequest, LoginResponse } from "@/api/auth";
-import { ReadBookmarksResponse } from "@/api/bookmark";
-import { bookmarkdb, createToken, userdb } from "@/mocks/utils";
+import { CreateBookmarkRequest } from "@/api/bookmark";
+import {
+  AddBookmarkToCollectionRequest,
+  CreateCollectionRequest,
+  ReadColletionRequest,
+  UpdateCollectionRequest,
+} from "@/api/collection";
+import {
+  bookmarkdb,
+  collectiondb,
+  createToken,
+  deleteBookmark,
+  deleteCollection,
+  findBookmarkById,
+  findCollectionById,
+  userdb,
+} from "@/mocks/utils";
 
 const serverUrl = import.meta.env.VITE_SERVER_URL;
 
 export const handlers = [
+  // auth
   http.post<PathParams, LoginRequest>(
     `${serverUrl}/api/v1/login`,
     async ({ request }) => {
@@ -44,7 +60,50 @@ export const handlers = [
     },
   ),
 
-  http.get<PathParams, DefaultBodyType, ReadBookmarksResponse>(
+  // bookmark
+  http.post<PathParams, CreateBookmarkRequest>(
+    `${serverUrl}/api/v1/users/:uuid/bookmarks`,
+    async ({ request }) => {
+      const { url, title, collectionId } = await request.json();
+
+      const boookmark = {
+        bookmarkId: bookmarkdb[bookmarkdb.length - 1].bookmarkId + 1,
+        url,
+        title,
+      };
+
+      bookmarkdb.push(boookmark);
+
+      if (collectionId) {
+        const collection = findCollectionById(collectionId);
+        collection?.bookmarks?.push({
+          bookmarkId: boookmark.bookmarkId,
+          title,
+          url,
+        });
+      }
+
+      return HttpResponse.json({
+        bookmarkId: boookmark.bookmarkId,
+        collectionId,
+        url,
+        title,
+      });
+    },
+  ),
+
+  http.delete<PathParams>(
+    `${serverUrl}/api/v1/users/:uuid/bookmarks/:bookmarkId`,
+    async ({ params }) => {
+      const { bookmarkId } = params;
+
+      deleteBookmark(+bookmarkId);
+
+      return HttpResponse.json({}, { status: 200 });
+    },
+  ),
+
+  http.get<PathParams, DefaultBodyType>(
     `${serverUrl}/api/v1/users/:uuid/bookmarks`,
     () => {
       return HttpResponse.json({
@@ -52,6 +111,138 @@ export const handlers = [
       });
     },
   ),
-];
 
-// `${import.meta.env.VITE_SERVER_URL}/api/v1/users/${request.uuid}/bookmarks`,
+  // collection
+  http.post<PathParams, CreateCollectionRequest>(
+    `${serverUrl}/api/v1/users/:uuid/collections`,
+    async ({ request }) => {
+      const { name, description } = await request.json();
+
+      const collection = {
+        collectionId: collectiondb[collectiondb.length - 1].collectionId + 1,
+        name,
+        description,
+        bookmarks: [],
+      };
+
+      collectiondb.push(collection);
+
+      return HttpResponse.json({}, { status: 201 });
+    },
+  ),
+
+  http.post<PathParams, AddBookmarkToCollectionRequest>(
+    `${serverUrl}/api/v1/users/:uuid/collections/:collectionId/bookmarks`,
+    async ({ params, request }) => {
+      const { collectionId } = params;
+      const { bookmarkId } = await request.json();
+
+      const collection = findCollectionById(+collectionId);
+      const bookmark = findBookmarkById(bookmarkId);
+
+      if (bookmark) {
+        collection?.bookmarks?.push(bookmark);
+        return HttpResponse.json({}, { status: 200 });
+      }
+
+      return HttpResponse.json({}, { status: 400 });
+    },
+  ),
+
+  http.get<PathParams, DefaultBodyType>(
+    `${serverUrl}/api/v1/users/:uuid/collections`,
+    () => {
+      return HttpResponse.json({
+        collections: collectiondb,
+      });
+    },
+  ),
+
+  http.get<PathParams, ReadColletionRequest>(
+    `${serverUrl}/api/v1/users/:uuid/collections/:collectionId`,
+    ({ params }) => {
+      const { collectionId } = params;
+
+      const collection = findCollectionById(+collectionId);
+
+      return HttpResponse.json({
+        ...collection,
+      });
+    },
+  ),
+
+  http.delete<PathParams>(
+    `${serverUrl}/api/v1/users/:uuid/collections/:collectionId`,
+    ({ params }) => {
+      const { collectionId } = params;
+
+      deleteCollection(+collectionId);
+
+      return HttpResponse.json({}, { status: 200 });
+    },
+  ),
+
+  http.put<PathParams, UpdateCollectionRequest>(
+    `${serverUrl}/api/v1/users/:uuid/collections/:collectionId`,
+    async ({ params, request }) => {
+      const { collectionId } = params;
+      const { name, description } = await request.json();
+
+      const index = collectiondb.findIndex(
+        c => c.collectionId === +collectionId,
+      );
+
+      if (index !== -1) {
+        collectiondb[index] = {
+          ...collectiondb[index],
+          name,
+          description,
+        };
+
+        return HttpResponse.json({}, { status: 200 });
+      }
+
+      return HttpResponse.json({}, { status: 400 });
+    },
+  ),
+
+  http.delete<PathParams>(
+    `${serverUrl}/api/v1/users/:uuid/collections/:collectionId/bookmarks/:bookmarkId`,
+    ({ params }) => {
+      const { collectionId, bookmarkId } = params;
+
+      const collectionIndex = collectiondb.findIndex(
+        c => c.collectionId === +collectionId,
+      );
+
+      if (collectionIndex !== -1) {
+        const bookmarkIndex = collectiondb[
+          collectionIndex
+        ].bookmarks!.findIndex(b => b.bookmarkId === +bookmarkId);
+
+        if (bookmarkIndex !== -1) {
+          collectiondb[collectionIndex].bookmarks?.splice(bookmarkIndex, 1);
+          return HttpResponse.json({}, { status: 200 });
+        }
+      }
+
+      return HttpResponse.json({}, { status: 400 });
+    },
+  ),
+
+  // verification
+  http.post(
+    `${serverUrl}/api/v1/check-duplicate-collection-name`,
+    ({ request }) => {
+      const url = new URL(request.url);
+
+      const collectionName = url.searchParams.get("name");
+
+      if (collectionName === "it") {
+        return HttpResponse.json({}, { status: 409 });
+      }
+
+      return HttpResponse.json({}, { status: 200 });
+    },
+  ),
+];
--- src/mocks/utils.ts
+++ src/mocks/utils.ts
@@ -2,19 +2,70 @@ export const createToken = () => Math.random().toString(36).substring(2, 11);
 
 export const userdb: { [key: string]: { email: string } } = {};
 
-export const bookmarkdb: Array<{
+export let bookmarkdb: Array<{
   bookmarkId: number;
   url: string;
   title: string;
 }> = [
   {
     bookmarkId: 1,
-    url: "www.example1.com",
-    title: "ex title 1",
+    url: "https://reactrouter.com/en/main",
+    title: "react router dom",
   },
   {
     bookmarkId: 2,
-    url: "www.example2.com",
-    title: "ex title 2",
+    url: "https://remix.run/",
+    title: "remix",
   },
 ];
+
+export let collectiondb: Array<{
+  collectionId: number;
+  name: string;
+  description: string;
+  bookmarks?: {
+    bookmarkId: number;
+    url: string;
+    title: string;
+  }[];
+}> = [
+  {
+    collectionId: 1,
+    name: "it",
+    description: "ex title 1",
+    bookmarks: [
+      {
+        bookmarkId: 1,
+        url: "https://reactrouter.com/en/main",
+        title: "react router dom",
+      },
+      {
+        bookmarkId: 2,
+        url: "https://remix.run/",
+        title: "remix",
+      },
+    ],
+  },
+  {
+    collectionId: 2,
+    name: "game",
+    description: "ex title 2",
+    bookmarks: [],
+  },
+];
+
+export const findCollectionById = (collectionId: number) => {
+  return collectiondb.find(c => c.collectionId === collectionId);
+};
+
+export const findBookmarkById = (bookmarkId: number) => {
+  return bookmarkdb.find(b => b.bookmarkId === bookmarkId);
+};
+
+export const deleteBookmark = (bookmarkId: number) => {
+  bookmarkdb = bookmarkdb.filter(b => b.bookmarkId !== bookmarkId);
+};
+
+export const deleteCollection = (collectionId: number) => {
+  collectiondb = collectiondb.filter(c => c.collectionId !== collectionId);
+};
