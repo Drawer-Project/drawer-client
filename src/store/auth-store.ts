5313097a342eedfc2307bbbe2943a898b4d1ac7a
Choi Hong Gyu <zxolqs@gmail.com>
2024-03-13 08:52:08 +0900

    refactor auth store

--- src/store/auth-store.ts
+++ src/store/auth-store.ts
@@ -1,49 +1,27 @@
 import { cookieService } from "@/modules/cookie";
 
-type User = {
-  accessToken: string;
-  uuid: string;
-  email: string;
-};
-
-const initUser: User = {
-  accessToken: "",
-  uuid: "",
-  email: "",
-};
-
 const PERIOD = 7;
 
 const authStore = {
-  saveUser: (user: User) => {
-    Object.entries(user).forEach(([key, value]) => {
-      const curDate = new Date();
-      const exprDate = new Date();
-      exprDate.setDate(curDate.getDate() + PERIOD);
-      cookieService.set(key, value, {
-        expires: exprDate,
-      });
+  saveToken: (token: string) => {
+    const curDate = new Date();
+    const exprDate = new Date();
+    exprDate.setDate(curDate.getDate() + PERIOD);
+    cookieService.set("accessToken", token, {
+      path: "/",
+      expires: exprDate,
     });
   },
 
-  getUser: () => {
-    return Object.keys(initUser).reduce((acc, key) => {
-      const value = cookieService.get(key)?.toString();
-      acc[key as keyof User] = typeof value === "string" ? value : "";
-      return acc;
-    }, {} as User);
-  },
-
-  removeUser: () => {
-    Object.keys(initUser).forEach(key => {
-      cookieService.remove(key);
+  removeToken: () => {
+    cookieService.remove("accessToken", {
+      path: "/",
     });
   },
 
-  getUuid: () => {
-    return cookieService.get("uuid");
+  getToken: () => {
+    return cookieService.get("accessToken");
   },
 };
 
-export type { User };
 export { authStore };
